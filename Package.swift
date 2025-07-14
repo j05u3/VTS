22efe208ec48007e81fc20e0c81103f0b03b4c8b
josue <josueajc@gmail.com>
2025-07-14 02:02:29 -0500

    simplification

--- Package.swift
+++ Package.swift
@@ -6,26 +6,8 @@ let package = Package(
     platforms: [
         .macOS(.v14)
     ],
-    products: [
-        .library(name: "VTS", targets: ["VTS"])
-    ],
     dependencies: [
         .package(url: "https://github.com/kishikawakatsumi/KeychainAccess", from: "4.2.2"),
         .package(url: "https://github.com/sindresorhus/KeyboardShortcuts", from: "2.0.0")
-    ],
-    targets: [
-        .target(
-            name: "VTS",
-            dependencies: [
-                "KeychainAccess",
-                "KeyboardShortcuts"
-            ],
-            path: "Sources/VTS"
-        ),
-        .testTarget(
-            name: "VTSTests",
-            dependencies: ["VTS"],
-            path: "Tests"
-        )
     ]
 )
